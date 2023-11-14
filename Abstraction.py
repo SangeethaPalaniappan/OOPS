ad6c0512c1674182522339ff62a4360cdfee0780
Sangeetha <sangeethapa2002@gmail.com>
2023-11-14 12:40:08 +0530

    Modified

--- Abstraction.py
+++ Abstraction.py
@@ -1,3 +1,7 @@
+# Import package 
+# If the child class has different implementations, Not same as the parent, at this time there is no use in implementing the parent class. This is abstraction
+# No implementation in parent class, but implementation for all the methods in the child class, if any of the method missed, then the child class also a abstract class 
+
 from abc import ABC, abstractclassmethod
 
 class animal(ABC):
--- Encapsulation.py
+++ Encapsulation.py
@@ -1,4 +1,7 @@
-# Encapsulation - Cannot access by the object
+# Encapsulation - Hide some variables and methods from outside the class by making it private, 
+# in python making private by, use double underscore infront of the variable or method name
+# even the object cannot access directly but can access through the public method or protector
+# It can access only inside the class
 
 class BankAccount:
     def __init__(self):
